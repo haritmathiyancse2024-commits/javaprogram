05a8e47dd1bea338b01be97c0b9049e4ff17cf28
haritmathiyancse2024-commits <harit.mathiyan.cse.2024@miet.ac.in>
2026-06-22 20:15:22 +0530

    Add files via upload

---
+++ righttriangle.java
@@ -0,0 +1,10 @@
+public class righttriangle {
+    static void main(String[] args) {
+        for(int i = 1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");//used print instead of println because it prints on the same line
+            }
+            System.out.println();
+        }
+    }
+}
