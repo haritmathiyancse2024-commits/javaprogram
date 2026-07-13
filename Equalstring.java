f11001b111af03940ff47d3c576a88b8f986b6d7
HARIT MATHIYAN <harit.mathiyan.cse.2024@miet.ac.in>
2026-07-13 10:44:28 +0530

    Add files via upload

---
+++ Equalstring.java
@@ -0,0 +1,7 @@
+public class Equalstring {
+    static void main(String[] args) {
+        String s1 = "HARIT";
+        String s2 = "mathiyan";
+        System.out.println(s1.equals(s2));
+    }
+}
